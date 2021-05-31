1acf0e2bc0db0c66c2ba417a6eba0620814beabe
SASergeevich <75212119+SASergeevich@users.noreply.github.com>
2021-05-31 10:25:35 +0300

    Update Bot1.py

--- Bot1.py
+++ Bot1.py
@@ -7,6 +7,15 @@ TOKEN = os.environ["TOKEN"]
 bot = telebot.TeleBot(TOKEN)
 
 
+@bot.message_handler(commands=["start"])
+def welcome(message):
+    sticker = open('img/sticker.webp', 'rb')
+    bot.send_sticker(message.chat.id, sticker)
+    bot.send_message(message.chat.id, "Добро пожаловать, {0.first_name}!\n"
+                     "Я - <b>{1.first_name}</b>".format(message.from_user, bot.get_me()),
+                     parse_mode="html")
+
+
 @bot.message_handler(content_types=['text'])
 def bla(message):
     bot.send_message(message.chat.id, message.text)
