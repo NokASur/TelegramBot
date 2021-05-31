0f7786b5fe40f2d45f7b32c2a119a0e9bfe82a3a
SASergeevich <75212119+SASergeevich@users.noreply.github.com>
2021-05-31 12:26:58 +0300

    Update Bot1.py

--- Bot1.py
+++ Bot1.py
@@ -1,6 +1,5 @@
 import os
 import telebot
-import random
 from telebot import types
 
 TOKEN = os.environ["TOKEN"]
@@ -12,22 +11,52 @@ bot = telebot.TeleBot(TOKEN)
 def welcome(message):
     sticker = open('img/sticker.webp', 'rb')
     bot.send_sticker(message.chat.id, sticker)
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    keyb = types.ReplyKeyboardMarkup(resize_keyboard=True)
     chat1 = types.KeyboardButton("Как дела?")
     chat2 = types.KeyboardButton("Какие новости?")
-    markup.add(chat1, chat2)
+    chat3 = types.KeyboardButton("♂Кто писал бо(y)ту♂")
+    keyb.add(chat1, chat2, chat3)
     bot.send_message(message.chat.id, "Добро пожаловать, {0.first_name}!\n"
                                       "Я - <b>{1.first_name}</b>".format(message.from_user, bot.get_me()),
-                     parse_mode="html", reply_markup=['text'])
+                     parse_mode="html", reply_markup=keyb)
 
 
 @bot.message_handler(content_types=['text'])
 def bla(message):
     if message.chat.type == "private":
         if message.text == "Как дела?":
-            bot.send_message(message.chat.id, "Отлично,{0.first_name}, как сам?")
+            ans = types.InlineKeyboardMarkup(row_width=2)
+            ans1 = types.InlineKeyboardButton("Хорошо", callback_data="good")
+            ans2 = types.InlineKeyboardButton("Плохо", callback_data="bad")
+            ans.add(ans1, ans2)
+            bot.send_message(message.chat.id,
+                             "Отлично, {0.first_name}, как сам?".format(message.from_user, bot.get_me()),
+                             reply_markup=ans)
+
+        elif message.text == "♂Кто писал бо(y)ту♂":
+            bot.send_message(message.chat.id, "Нельзя просто так взять и реализовать базу данных")
+
+        elif message.text == "Какие новости?":
+            bot.send_message(message.chat.id, "")
+
         else:
-            bot.send_message(message.chat.id, "Иди лучше решай серии, а не вбрасывай сюда всякую фигню,{0.first_name}")
+            bot.send_message(message.chat.id,
+                             "Иди лучше решай серии, а не вбрасывай сюда всякую фигню, {0.first_name}".format(
+                                 message.from_user, bot.get_me()))
+
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback(call):
+    try:
+        if call.message:
+            if call.data == "good":
+                bot.send_message(call.message.chat.id, 'Я очень этому рад')
+            elif call.data == "bad":
+                bot.send_message(call.message.chat.id, 'Очень жаль')
+            bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text="Как дела?",
+                                  reply_markup=None)
+    except Exception as e:
+        print(repr(e))
 
 
 bot.polling(none_stop=True)
